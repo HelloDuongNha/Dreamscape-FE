d7939e71e73408eaadc2a40104dfbc71137cea1f
HelloDuongNha <hoangduongbi6th@gmail.com>
2026-07-30 09:12:21 +0700

    feat: add Firebase configuration and socket URL update, and ignore .vercel directory

--- src/store/useChatStore.ts
+++ src/store/useChatStore.ts
@@ -177,7 +177,7 @@ export const useChatStore = defineStore('chat', () => {
       socket = null
     }
 
-    socket = io(BACKEND_URL, {
+    socket = io(SOCKET_URL, {
       auth:        { token },
       transports:  ['websocket', 'polling'],
       autoConnect: true,
